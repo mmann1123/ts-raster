ee908b9620fa04ead18884885dd2ae14a18b5f88
adbeda <destaneon@gmail.com>
2018-09-16 10:53:09 -0400

    update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/adbeda/ts-raster'
 EMAIL = 'adbe@gwu.edu'
 AUTHOR = 'Adane Bedada'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 0.1
+VERSION = 0.12
 
 
 # What packages are required for this module to be executed?
