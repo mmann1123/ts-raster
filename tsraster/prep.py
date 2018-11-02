723a1baea0d4725ba3838022572702d7a3032501
Michael Mann <MMann@cloud.ccas.gwu.edu>
2018-11-02 11:21:48 -0400

    fixed mask generator added mask properties check

--- tsraster/prep.py
+++ tsraster/prep.py
@@ -8,6 +8,9 @@ import glob
 import os.path
 import pandas as pd
 from tsfresh import extract_features
+import geopandas as gpd
+import rasterio
+from rasterio import features
 
 try:
     import gdal
@@ -158,45 +161,75 @@ class sRead:
         return df
     
     def poly_rasterizer(poly,raster_ex, raster_path_prefix, buffer_poly_cells=0):
-    '''
-    :poly_rasterizer: Function rasterizes polygons assigning the value 1, it \\
-    can also add a buffer at a distance that is multiples of the example raster resolution
-    :param raster_path_prefix: full path and prefix for raster name 
-    :param buffer_poly_cells: int specifying number of cells to buffer polygon with, 0 for no buffer
-    :return: raster
-     '''
-     
-    # check if polygon is already geopandas dataframe if so, don't read again
-    if ('poly' in locals()):
-        if not(isinstance(poly, gpd.geodataframe.GeoDataFrame)): 
-            poly = gpd.read_file(poly)
-    else:
-        poly = poly
-    
-    # create column of ones to rasterize for presence (1) of fire
-    poly['ONES'] = 1
-       
-    # get example metadata
-    with rasterio.open(raster_ex) as src:
-        array = src.read()
-        profile = src.profile
-        profile.update(dtype=rasterio.float32, count=1, compress='lzw',nodata=0)
-        out_arr = src.read(1) # get data from first band, this gets updated in write
-        out_arr.fill(0) #set all values of raster to zero 
+        '''
+        :poly_rasterizer: Function rasterizes polygons assigning the value 1, it \\
+        can also add a buffer at a distance that is multiples of the example raster resolution
+        :param raster_path_prefix: full path and prefix for raster name 
+        :param buffer_poly_cells: int specifying number of cells to buffer polygon with, 0 for no buffer
+        :return: raster
+         '''
+         
+        # check if polygon is already geopandas dataframe if so, don't read again
+        if ('poly' in locals()):
+            if not(isinstance(poly, gpd.geodataframe.GeoDataFrame)): 
+                poly = gpd.read_file(poly)
+        else:
+            poly = poly
         
-    # reproject polygon to match crs of raster
-    poly = poly.to_crs(src.crs)
-    
-    # buffer polygon to avoid edge effects 
-    if buffer_poly_cells != 0:
-        poly['geometry'] = poly.buffer(buffer_poly_cells*src.res[0] ) # this creates an empty polygon geoseries
+        # create column of ones to rasterize for presence (1) of fire
+        poly['ONES'] = 1
+           
+        # get example metadata
+        with rasterio.open(raster_ex) as src:
+            array = src.read()
+            profile = src.profile
+            profile.update(dtype=rasterio.float32, count=1, compress='lzw',nodata=0)
+            out_arr = src.read(1) # get data from first band, this gets updated in write
+            out_arr.fill(0) #set all values of raster to zero 
+            
+        # reproject polygon to match crs of raster
+        poly = poly.to_crs(src.crs)
         
-    # Write to tif, using the same profile as the source
-    with rasterio.open(raster_path_prefix+'.tif', 'w', **profile) as dst:
-
-        # generator of geom, value pairs to use in rasterizing
-        shapes = ((geom,value) for geom, value in zip(poly.geometry, poly.ONES))
-
-        #rasterize shapes 
-        burned_value = features.rasterize(shapes=shapes, fill=0, out=out_arr, transform=dst.transform)
-        dst.write(burned_value,1)
+        # buffer polygon to avoid edge effects 
+        if buffer_poly_cells != 0:
+            poly['geometry'] = poly.buffer(buffer_poly_cells*src.res[0] ) # this creates an empty polygon geoseries
+            
+        # Write to tif, using the same profile as the source
+        with rasterio.open(raster_path_prefix+'.tif', 'w', **profile) as dst:
+    
+            # generator of geom, value pairs to use in rasterizing
+            shapes = ((geom,value) for geom, value in zip(poly.geometry, poly.ONES))
+    
+            #rasterize shapes 
+            burned_value = features.rasterize(shapes=shapes, fill=0, out=out_arr, transform=dst.transform)
+            dst.write(burned_value,1)
+            
+            
+            
+    def check_mask(raster_mask,raster_input_ex):
+        '''
+        :check_mask: Checks that mask and input rasters have identical properties
+        :param raster_mask: full path and prefix for raster name 
+        :raster_input_ex raster_ex: int specifying number of cells to buffer polygon with, 0 for no buffer
+        :return: raster
+        '''
+        mask_list = []
+        ex_list = []
+        test_list = ['Mask','Resolution','Bounds']
+        
+        with rasterio.open(raster_mask) as mask:
+            mask_list = [mask.crs,mask.res,mask.bounds]
+         
+        with rasterio.open(raster_input_ex) as ex:
+            ex_list = [ex.crs,ex.res,ex.bounds]
+         
+        for i in range(0,len(mask_list)):
+            if mask_list[i] == ex_list[i]:
+                print(test_list[i]+": passed")
+            else:
+                print(test_list[i]+": FAILED")
+        
+        # close rasters
+        mask.close()        
+        ex.close()                    
+            
