34f514a510bc85824b83c00136dc2e9f43b66bbf
adbeda <destaneon@gmail.com>
2018-11-03 18:21:56 -0400

    updated test scripts

--- tests/checkRelevance.py
+++ tests/checkRelevance.py
@@ -1,16 +1,17 @@
 
 '''this test script returns p=value showing the significance of features '''
 
-from tsraster.calculate import checkRelevance
+from tsraster.calculate import checkRelevance2
+from tsraster.prep import targetData
+import pandas as pd
 
 # read csv extracted features
-features = pd.read_csv("test_features.csv")
-
+features = pd.read_csv("../../extracted_features.csv")
 #path to target data
 target_variable = "../docs/img/target_data/"
-
 #read target data
-target_data = sRead.targetData(target_variable)
-
+target_data = targetData(target_variable)
 #check for relevance
-relevance = checkRelevance(features, target_data)
+relevance = checkRelevance2(features, target_data)
+
+print(relevance)
