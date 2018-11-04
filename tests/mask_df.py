2fa3796e579e033c4474a0b53e7d162176362ac1
adbeda <destaneon@gmail.com>
2018-11-04 18:12:06 -0500

    updated test script for masking and unmasking

--- tests/mask_df.py
+++ tests/mask_df.py
@@ -1,14 +1,22 @@
 from tsraster.prep import mask_df, unmask_df, check_mask
 import pandas as pd
+
 #path to files
 raster_mask="../../wildfire_FRAP/Data/Examples/3month/aet-198401.tif"
 
 original_df ="../../wildfire_FRAP/Data/Examples/3month/my_df.csv"
 
-#run task
+#run tasks
 masked_data = mask_df(raster_mask, original_df)
 
-
+#check maksing
 original_df_file = pd.read_csv(original_df)
-print("Size of unmasked data: ", original_df_file.shape)
+print("Check Masking")
+print("Size of original data: ", original_df_file.shape)
 print("Size of masked data: ", masked_data.shape)
+
+print("Check Unmasking")
+#check unmaksing
+unmasked_data = unmask_df(original_df, masked_data)
+print("Size of original data: ", original_df_file.shape)
+print("Size of unmasked data: ", unmasked_data.shape)
